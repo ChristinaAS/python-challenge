ad50380f41730e9765adeb4671348bfa11d29322
ChristinaAS <smiley9824@aol.com>
2020-06-29 21:05:03 -0400

    push work

---
+++ PyBank/mainPyBank.py
@@ -0,0 +1,68 @@
+import os
+import csv
+
+budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+OutPutPath = os.path.join("..", "Resources", "Budgetanalysis.txt")
+totalPL = 0
+totalmonths = 0
+aveRevChange = []
+lastMonthPL = 0
+greatestinc = 0
+greatestincMonth = None
+changes = []
+greatestdec = 0
+revChange = 0
+diff = 0
+# Open and read csv
+with open(budget_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvreader)
+    firstrow = next(csvreader)
+    totalmonths = totalmonths + 1
+    totalPL += int(firstrow[1])
+    lastMonthPL = int(firstrow[1])
+    
+    for row in csvreader:
+        
+        #count number of months
+        totalmonths = totalmonths + 1
+
+        #add the total amount of "Profit/Losses"
+        totalPL += int(row[1])
+
+        #find the difference in values between months
+        diff = int(row[1]) - lastMonthPL
+                  
+        #find the month with the greatest increase in profits
+        if diff > greatestinc:
+            greatestinc = diff
+            greatestincMonth = row[0]
+        
+        #find the month with the greatest decrease in profits
+        if diff < greatestdec:
+            greatestdec = diff
+            greatestdecMonth = row[0]
+        
+        aveRevChange.append(diff)
+        lastMonthPL = int(row[1])
+   
+        
+
+#print evertyhing
+print("Financial Analysis")
+print("--------------------------------------------------") 
+print(f"Total Months: {totalmonths}")
+print(f"Total Profit/Loss: ${totalPL}")
+print(f"Avg. Change: ${round(sum(aveRevChange)/len(aveRevChange),2)}")
+print(f"Month with Greatest Increase: {greatestincMonth} ${greatestinc} ")
+print(f"Greatest Decrease in Profit/Loss: {greatestdecMonth} ${greatestdec}")
+
+with open(OutPutPath, "w") as file:
+    file.write("Financial Analysis\n")
+    file.write("-------------------------------------------------\n") 
+    file.write(f"Total Months: {totalmonths} \n")
+    file.write(f"Total Profit/Loss: ${totalPL} \n")
+    file.write(f"Avg. Change: ${round(sum(aveRevChange)/len(aveRevChange),2)} \n")
+    file.write(f"Month with Greatest Increase: {greatestincMonth} ${greatestinc} \n")
+    file.write(f"Greatest Decrease in Profit/Loss: {greatestdecMonth} ${greatestdec}")
+    file.close
---
+++ PyPoll/mainPyPoll.py
@@ -0,0 +1,71 @@
+import os
+import csv
+
+
+totalvotes = 0
+can_list = []
+candidatevotes = {}
+winning_candidate = ""
+winning_count = 0
+
+
+#Import and export paths
+election_csv = os.path.join('..', 'Resources', 'election_data.csv')
+OutPutPath = os.path.join("..", "Resources", "Pollanalysis.txt")
+
+#open and read csv
+with open(election_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvreader)
+
+    for row in csvreader:
+        
+        #Count the votes
+        totalvotes += 1
+
+        # Pick out the candidates
+        candidatename = row[2]
+        if candidatename in can_list:
+            candidatevotes[candidatename] = candidatevotes[candidatename] + 1
+            # Add new candidate to the list
+        else:
+            can_list.append(candidatename)
+            candidatevotes[candidatename]= 1
+    
+    #create a loop to count percentages for each candidate
+    for candidate in candidatevotes:
+        votes = candidatevotes[candidate]
+        votepercentage = float(votes)/float(totalvotes)
+        votepercentage = "{:.2%}".format(votepercentage)
+        
+        if votes > winning_count:
+            winning_count = votes
+            winning_candidate = candidate
+         
+
+#print evertyhing
+print("Election Results")
+print("--------------------------------------------------") 
+print(f"Total Votes: {totalvotes}")
+print("--------------------------------------------------")
+for candidate in candidatevotes:
+    votes = candidatevotes[candidate]
+    votepercentage = float(votes)/float(totalvotes)
+    votepercentage = "{:.2%}".format(votepercentage)
+    print(candidate + ": " + str(candidatevotes[candidate]) + " (" + str(votepercentage) + ")")
+print("---------------------------------------------------")
+print("Winner " + (winning_candidate))
+
+with open(OutPutPath, "w") as file:
+    file.write("Election Results \n")
+    file.write("----------------------------------------------------\n") 
+    file.write(f"Total Votes: {totalvotes} \n")
+    file.write("----------------------------------------------------\n")
+    for candidate in candidatevotes:
+        votes = candidatevotes[candidate]
+        votepercentage = float(votes)/float(totalvotes)
+        votepercentage = "{:.2%}".format(votepercentage)
+        file.write(candidate + ": " + str(candidatevotes[candidate]) + " (" + str(votepercentage) + ") \n")
+    file.write("-----------------------------------------------------\n")
+    file.write("Winner " + (winning_candidate))
+    file.close
